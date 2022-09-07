c1c53599fbc11ce896867477011f330a77d295d1
Antonio Nicasio <webmediadigital@gmail.com>
2022-09-07 16:22:51 -0700

    initial base configuration jsapp from scratch

---
+++ webpack.common.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HtmlWebpackPlugin = requite("htm-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        type: "asset",
+      },
+    ],
+  },
+};
